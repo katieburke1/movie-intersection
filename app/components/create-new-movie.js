00b250767b2c911006096c04ad1f4be544123241
katieburke1 <katieburke74@gmail.com>
2016-11-20 18:14:37 +0000

    Add styles

--- app/components/create-new-movie.js
+++ app/components/create-new-movie.js
@@ -3,10 +3,15 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     createMovie: function (model) {
-          console.log("Hello!");
-          console.log(model.title);
-          this.sendAction('createMovie', model);
-        },
+      console.log("Hello!");
+      console.log(model.title);
+      this.sendAction('createMovie', model);
+          // Clear each input field
+      this.set('model.title', null);
+      this.set('model.year', null);
+      this.set('model.imdbrating', null);
+    },
+
     deleteMovie: function (model) {
       this.sendAction('deleteMovie', model);
     }
