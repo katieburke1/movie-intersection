d86d6801bdd860e432a0bd0ef589592717aeff9e
katieburke1 <katieburke74@gmail.com>
2016-12-17 16:18:47 +0000

    Tidying some code

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   host: 'http://localhost:3000'
+  // host: 'http://www.movieintersection.com/api'
   // Application specific overrides go here
 });
--- app/routes/movies/new.js
+++ app/routes/movies/new.js
@@ -3,16 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return {};
-    // return Ember.$.ajax({
-    //         type: 'POST',
-    //         url: 'http://localhost:3000/movies',
-    //         contentType: 'application/json',
-    //         data: JSON.stringify({
-    //           title: "Best Movie Ever",
-    //           year: "2011",
-    //           imdbrating: "9.5"
-    //         })
-    //     });
   },
 
   actions: {
